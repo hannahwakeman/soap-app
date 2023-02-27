69b141dafaac153fc9ceb44602565184e9d2e6c0
hnnnhl <84357035+hnnnhl@users.noreply.github.com>
2023-02-26 22:53:17 -0500

    Update ShopGridItem.tsx

--- src/components/shop/ShopGridItem.tsx
+++ src/components/shop/ShopGridItem.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ShopItem } from "../../Interfaces";
 import "./ShopGridItem.scss";
 import ShopItemDetailsModal from "./ShopItemDetailsModal";
